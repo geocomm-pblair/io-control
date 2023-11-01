dd356a266d46d2b6ebf3b8abb6187bb76419d001
Pat Blair <pblair@geo-comm.com>
2023-11-01 14:13:55 -0500

    wip: Refactoring provisioning.

--- iocontrol/tenants/planning/aws.py
+++
@@ -1,7 +0,0 @@
-from iocontrol.tenants.models.plans import Plan
-from iocontrol.tenants.planning.base import Planner
-
-
-class AwsPlanner(Planner):
-    def create(self, *args, **kwargs) -> Plan:
-        pass
